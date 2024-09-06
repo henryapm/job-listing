db37ef16f95b7d8f829e4fb441673305f2cb7dc3
Henry Paredes <henryalexander24@gmail.com>
2024-09-06 19:35:21 -0400

    responsive design fix test

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,9 @@ module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
     ? '/job-listing/'  // Replace 'your-repo-name' with the actual repository name
-    : '/'
+    : '/',
+    devServer: {
+      port: 8080, // or any available port
+      open: true, // to auto-open the browser
+    }
 })
